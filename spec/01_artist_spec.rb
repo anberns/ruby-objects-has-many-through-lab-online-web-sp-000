1ed572f193424c52d9af8bab466457d808a6deb6
Aaron Berns <anberns@gmail.com>
2019-01-12 18:28:03 +0000

    Automatically backed up by Learn

--- spec/01_artist_spec.rb
+++ spec/01_artist_spec.rb
@@ -49,7 +49,6 @@ describe "Artist" do
       ninety_nine_problems = jay_z.new_song("Ninety Nine Problems", rap)
 
       expect(jay_z.genres).to include(rap)
-      puts jay_z.songs.name 
       expect(jay_z.songs.first.genre).to eq(rap)
     end
   end
