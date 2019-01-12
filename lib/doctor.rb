1c2998bf9d85b2a07ba639f012edd974be18012c
Aaron Berns <anberns@gmail.com>
2019-01-12 18:37:07 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,37 +14,19 @@ class Doctor
   end 
 
   def new_appointment(patient, date)
-    Appointment.new(
+    Appointment.new(patient, self, date) 
+  end 
 
-
-  describe '#new_appointment' do
-    it 'given a date and a patient, creates a new appointment' do
-      doctor_who = Doctor.new('The Doctor')
-      hevydevy = Patient.new('Devin Townsend')
-      appointment = doctor_who.new_appointment(hevydevy, 'Friday, January 32nd')
-      expect(doctor_who.appointments).to include(appointment)
-      expect(appointment.doctor).to eq(doctor_who)
-    end
-  end
-
-  describe '#appointments' do
-    it 'has many appointments' do
-      doctor_who = Doctor.new('The Doctor')
-      hevydevy = Patient.new('Devin Townsend')
-      doctor_who.new_appointment(hevydevy, 'Friday, January 32nd')
-      appointment = doctor_who.new_appointment(hevydevy, 'Saturday, January 33rd')
-      doctor_who.new_appointment(hevydevy, 'Rootenskadootenday, January 315th')
-      expect(doctor_who.appointments).to include(appointment)
-    end
-  end
-
-  describe '#patients' do
-    it 'has many patients, through appointments' do
-      doctor_who = Doctor.new('The Doctor')
-      hevydevy = Patient.new('Devin Townsend')
-      doctor_who.new_appointment(hevydevy, 'Friday, January 32nd')
-
-      expect(doctor_who.patients).to include(hevydevy)
-    end
-  end
-end
+  def appointments
+    Appointment.all.select do |appt|
+      appt.doctor == self 
+    end 
+  end 
+  
+  def patients 
+    appointments.collect do |appt|
+      appt.patient 
+    end 
+  end 
+  
+end 
